2858ec10e893bcba28a028a24ef9e410a6632ca2
Javi <monmany1992@gmail.com>
2023-04-11 18:24:15 +0200

    Add styles v1.0

--- src/components/Picture.vue
+++ src/components/Picture.vue
@@ -1,13 +1,37 @@
 <template>
-    <h1>Picture</h1>
+  <div class="pokemon-container">
+    <img
+      src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg"
+      alt="pokemon"
+      class="fade-in"
+    />
+    <img
+      src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg"
+      alt="pokemon"
+      class="hidden"
+    />
+  </div>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
-<style>
-
+<style scoped>
+img {
+  height: 200px;
+  position: absolute;
+  right: 44%;
+  user-select: none;
+  -moz-user-select: none;
+  -ms-user-select: none;
+  -webkit-user-drag: none;
+  -webkit-user-select: none;
+}
+.hidden {
+  filter: brightness(0);
+}
+.pokemon-container {
+  height: 200px;
+}
 </style>
