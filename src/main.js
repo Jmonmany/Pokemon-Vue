8b08222dbe0ee394c892d2af372cccfecfab9cd5
Javi <monmany1992@gmail.com>
2023-04-11 18:10:40 +0200

    Initial commit

--- src/main.js
+++ src/main.js
@@ -1,4 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import './css/styles.css'
+import "./css/animation.css";
 
 createApp(App).mount('#app')
---
+++ src/pages/PokemonPage.vue
@@ -0,0 +1,19 @@
+<template>
+  <h1>Who is this Pokemon?</h1>
+  <Picture />
+
+  <Options />
+</template>
+
+<script>
+import Options from "@/components/Options.vue";
+import Picture from "@/components/Picture.vue";
+export default {
+  components: {
+    Options,
+    Picture,
+  },
+};
+</script>
+
+<style></style>
